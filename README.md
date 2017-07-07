fbe8edb6cd85076c48797a3d737b3936f9eb327e
Stuart Charlton <scharlton@pivotal.io>
2017-07-07 09:39:29 +0800

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # cycle-bindings
-Unbinds and rebinds, or restarts, CF service bindings
+Unbinds and rebinds, or restarts apps with given CF service bindings.  Useful when you need to re-IP your services.
+
+Prereqs:  the CF CLI logged in as an admin user; the jq command must also be installed
+This command does not actually execute any CF commands, it just echos them.   Redirect the output to a file and then execute it.
+
+How to use:
+```
+rebind.sh [restart|rebind] [service offering name (default all)] > my_rebind_script.sh
+chmod u+x ./my_rebind_script.sh
+./my_rebind_script.sh
+```
+Docs:
+- *Restart* will restart all apps that are bound to a service of a given name in the marketplace (or , default, all services)
+- *Rebind* will unbind and rebind all apps that are bound to a service of a given name in the marketplace
+
+Example:
+
+Rebind all p-mysql instances
+./rebind.sh rebind p-mysql > rebind_mysql.sh
+chmod u+x ./rebind_mysql.sh 
+./rebind_mysql.sh
+
+
---
+++ rebind.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+if [[ ($1 == "rebind") || ($1 == "restart" ) ]]
+then
+  command=$1
+  echo "# $command bound services"
+else
+  echo "USAGE: rebind.sh [rebind|restart] [service name (default all)]"
+  exit 1
+fi
+
+if [ -z "$2" ]; then
+  use_service_name=all
+else
+  use_service_name=$2
+fi
+
+if [[ "$(which jq)X" == "X" ]]; then
+  echo "Please install jq"
+  exit 1
+fi
+if [[ "$(which cf)X" == "X" ]]; then
+  echo "Please install cf"
+  exit 1
+fi
+
+service_bindings=$(cf curl /v2/service_bindings | jq -r -c .resources[])
+# TODO: replace 'in progress' with 'in-progress' before splitting as an array
+for service_binding in $service_bindings; do
+        binding_guid=$(echo $service_binding | jq -r .metadata.guid)
+        app_url=$(echo $service_binding | jq -r .entity.app_url)
+        app_name=$(cf curl $app_url | jq -r .entity.name)
+        app_guid=$(cf curl $app_url | jq -r .metadata.guid)
+        app_space_url=$(cf curl $app_url | jq -r .entity.space_url)
+        service_instance_url=$(echo $service_binding | jq -r .entity.service_instance_url)
+        service_instance_name=$(cf curl $service_instance_url | jq -r .entity.name)
+        service_url=$(cf curl $service_instance_url | jq -r .entity.service_url)
+        service_name=$(cf curl $service_url | jq -r .entity.label)
+        echo "# Service_name $service_name , service_instance $service_instance_name " 
+        if [[ ($use_service_name == "all") || ($use_service_name == $service_name) ]]; then
+        
+          space_name=$(cf curl $app_space_url | jq -r .entity.name)
+          org_url=$(cf curl $app_space_url | jq -r .entity.organization_url)
+          org_name=$(cf curl $org_url | jq -r .entity.name)
+
+          if [[ ($command == "rebind") ]]; then
+            echo "# Cycling binding on app $app_name from service $service_instance_name in $org_name/$space_name"
+            recreation_command=("cf target -o $org_name -s $space_name; cf bs $app_name $service_instance_name")
+            echo "cf curl -X DELETE \"/v2/apps/$app_guid/service_bindings/$binding_guid\"" || echo "Failed to unbind for some reason."
+            echo "$recreation_command" 
+#            eval $recreation_command
+          fi
+          if [[ ($command == "restart") ]]; then
+            echo "# Restarting app $app_name bound to service $service_instance_name in $org_name/$space_name"
+            recreation_command=("cf target -o $org_name -s $space_name; cf restart $app_name")
+            echo "$recreation_command"
+#           eval $recreation_command
+          fi     
+        fi
+done
+
